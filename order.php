7647c4335c6407027eee8c7cb160056e3a088a83
mazahir7 <mazahir1220@gmail.com>
2023-04-16 16:08:28 +0530

    Complete!!!

---
+++ order.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $fname = $_POST["fname"];
+  $lname = $_POST["lname"];
+  $item = $_POST["product"];
+  $phoneno = $_POST["phoneno"];
+
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "bakery";
+
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+
+  $sql = "INSERT INTO orderlist (firstname, lastname, item, phoneno) VALUES ('$fname', '$lname', '$item', '$phoneno')";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "Order placed successfully.";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+?>
